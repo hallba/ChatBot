61aa7ed9efa28ed810381dc978e27a26842eda4d
Ben Hall <BH418@Hutchison-MRC.cam.ac.uk>
2018-09-20 16:21:07 +0100

    Altered dialog structure to allow further testing

--- src/dialogs/stability.ts
+++ src/dialogs/stability.ts
@@ -11,30 +11,50 @@ import {receiveModelAttachmentStep} from './modelStorage'
 import * as strings from './strings'
 
 export function registerStabilityDialog (bot: builder.UniversalBot, modelStorage: ModelStorage, skipBMAAPI: boolean) {
-    bot.dialog('/stability', [
-        //Add check for BMA API availability
+    bot.dialog('/checkModel', [
         (session, args, next) => {
-            session.send(strings.PROTOTYPE_WARNING)
-            let text = args
-            // check if JSON model has been uploaded already, otherwise prompt user
             if (!session.conversationData.bmaModel) {
-                session.conversationData.lastMessageText = text
-                session.save()
                 builder.Prompts.attachment(session, strings.MODEL_SEND_PROMPT)
             } else {
-                testStability(session, text, modelStorage)
                 session.endDialog()
             }
         },
         (session, results, next) => receiveModelAttachmentStep(bot, modelStorage, session, results, next),
+        (session, results, next) => session.endDialog()
+    ])
+    bot.dialog('/furtherTesting', [
+        (session, args, next) => builder.Prompts.confirm(session, strings.STABILITY_INCONCLUSIVE),
+        (session, results: builder.IPromptChoiceResult, next) => {
+            if (results.response) {
+                furtherTesting(session)
+                session.send(strings.PROTOTYPE_INCOMPLETE)
+                session.endDialog()
+            } else {
+                session.send(strings.OK)
+                session.endDialog()
+            }
+        }
+    ])
+    bot.dialog('/stability', [
+        //Add check for BMA API availability
+        (session, args, next) => {
+            session.send(strings.PROTOTYPE_WARNING)
+            // check if JSON model has been uploaded already, otherwise prompt user
+            if (!session.conversationData.bmaModel) {
+                session.beginDialog('/checkModel')
+                next()
+            } else {
+                next()
+            }
+        },
         (session, results, next) => {
-            testStability(session, session.conversationData.lastMessageText, modelStorage)
-            delete session.conversationData.lastMessageText
+            testStability(session)
+            session.endDialog()
         }
     ])
 }
 
-function testStability (session: builder.Session, text: string, modelStorage: ModelStorage) {
+function testStability (session: builder.Session) {
     // fetch some session state
     let bmaModel: BMA.ModelFile = session.conversationData.bmaModel
 
@@ -43,17 +63,17 @@ function testStability (session: builder.Session, text: string, modelStorage: Mo
     //session.send('The model is :' + response.Status)
     if (response.Status == "Stabilizing") {
         session.send(strings.STABILITY_PROVED)
-        session.endDialog()
     } else if (response.Status = "NotStabilizing") {
-        session.send(strings.STABILITY_INCONCLUSIVE)
-        //builder.Prompts.confirm(session, strings.STABILITY_INCONCLUSIVE)
-        session.send(strings.PROTOTYPE_INCOMPLETE)
-        session.endDialog()
+        session.beginDialog('/furtherTesting')
     } else {
         session.send(strings.BAD_RESULT)
-        session.endDialog()
     }
 
     })
     //session.send(strings.PROTOTYPE_INCOMPLETE)
+}
+
+function furtherTesting (session: builder.Session) {
+    let bmaModel: BMA.ModelFile = session.conversationData.bmaModel
+
 }
