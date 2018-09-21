a1725613591793cbee7c73597f4ced3d69e711e3
Ben Hall <a-benha@microsoft.com>
2018-09-21 11:01:45 +0100

    more informative initial proof result

--- src/dialogs/strings.ts
+++ src/dialogs/strings.ts
@@ -106,6 +106,6 @@ export let INCREASE_STEPS = 'Increasing the number of steps will give you a broa
 
 //Stability proof
 export let STABILITY_PROVED = 'The model was proved to be stable.'
-export let STABILITY_INCONCLUSIVE = 'The stability proof was inconclusive. Would you like to perform further testing?'
+export let STABILITY_INCONCLUSIVE = 'The fast stability check was inconclusive. Would you like to perform further testing (slower)?'
 export let FOUND_CYCLE = (steps:number) => `There is a cycle of length ${steps}.`
 export let FOUND_BIFURCATION = 'There is a bifurcation.'
